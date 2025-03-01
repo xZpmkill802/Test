2c71e4683935e25b929edeecdbd38ef1e6803402
xZpmkill802 <xzpmkillz@gmail.com>
2025-03-01 18:06:13 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,7 @@
+local key = _G.Key
+local check = "https://raw.githubusercontent.com/xZpmkill802/Test/refs/heads/main/Check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xZpmkill802/Test/refs/heads/main/xVbm-Hub-Blue-lock%3A-Rivals.lua"))()
+else
+game.Players.LocalPlayer:Kick("Key ไม่ถูก มึงโดนแบนจากแมพนี้!")
+end
